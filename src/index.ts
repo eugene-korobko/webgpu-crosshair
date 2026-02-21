fdb292df1e334c50c7f91243757b73f73c09881c
Eugene Korobko <ekorobko@tradingview.com>
2026-02-21 22:03:57 +0300

    First render

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,97 @@
 import fragmentShaderSource from './shader.glsl';
 
-console.log("Shader Source Loaded:", fragmentShaderSource);
-// Initialize WebGL and use fragmentShaderSource here...
-const canvas = document.getElementById('glcanvas') as HTMLCanvasElement;
-console.log('Canvas ready', canvas);
+type DeepPartial<T> = T extends object ? {
+    [P in keyof T]?: DeepPartial<T[P]>;
+} : T;
+
+async function init(): Promise<void> {
+	const canvas = document.getElementById('glcanvas') as HTMLCanvasElement;
+
+	const adapter = await navigator.gpu?.requestAdapter();
+	const device = await adapter?.requestDevice();
+	if (!device) {
+		console.error('need a browser that supports WebGPU');
+		return;
+	}
+
+	const context = canvas.getContext('webgpu') as GPUCanvasContext;
+	if (!context) {
+		console.error('cannot get webgpu context');
+		return;
+	}
+	const presentationFormat = navigator.gpu.getPreferredCanvasFormat();
+	context.configure({
+		device,
+   		format: presentationFormat,
+	});
+
+	const module = device.createShaderModule({
+		label: 'our hardcoded red triangle shaders',
+		code: /* wgsl */ `
+		  @vertex fn vs(
+			@builtin(vertex_index) vertexIndex : u32
+		  ) -> @builtin(position) vec4f {
+			let pos = array(
+			  vec2f( 0.0,  0.5),  // top center
+			  vec2f(-0.5, -0.5),  // bottom left
+			  vec2f( 0.5, -0.5)   // bottom right
+			);
+	 
+			return vec4f(pos[vertexIndex], 0.0, 1.0);
+		  }
+	 
+		  @fragment fn fs() -> @location(0) vec4f {
+			return vec4f(1.0, 0.0, 0.0, 1.0);
+		  }
+		`,
+	});
+
+	const pipeline = device.createRenderPipeline({
+		label: 'our hardcoded red triangle pipeline',
+		layout: 'auto',
+		vertex: {
+		  module,
+		  entryPoint: 'vs',
+		},
+		fragment: {
+		  module,
+		  entryPoint: 'fs',
+		  targets: [{ format: presentationFormat }],
+		},
+	});
+
+	const renderPassDescriptor = {
+		label: 'our basic canvas renderPass',
+		colorAttachments: [
+		  {
+			// view: <- Чтобы текстура была заполнена внутри
+			clearValue: [0.3, 0.3, 0.3, 1],
+			loadOp: 'clear',
+			storeOp: 'store',
+		  },
+		],
+	}as GPURenderPassDescriptor;
+
+	function render() {
+        // Получаем текущую текстуру из canvas context и устанавливаем ее как текстуру для рендеринга
+        (renderPassDescriptor.colorAttachments as GPURenderPassColorAttachment[])[0].view =
+            context!.getCurrentTexture().createView();
+     
+        // создаем шаблон команды, чтобы запускать их
+        const encoder = device!.createCommandEncoder({ label: 'our encoder' });
+     
+        // создаем render pass encoder для установке нашего шаблона
+        const pass = encoder.beginRenderPass(renderPassDescriptor);
+        pass.setPipeline(pipeline);
+        pass.draw(3);  // вызываем наш vertex shader три раза
+        pass.end();
+     
+        const commandBuffer = encoder.finish();
+        device!.queue.submit([commandBuffer]);
+      }
+     
+      render();
+}
+
+init();
+
